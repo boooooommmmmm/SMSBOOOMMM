d85b8c73fd61ca31d2e01348573f8e6eaf1cddd7
boooooommmmmm <hushuyan88@gmail.com>
2018-06-28 23:34:08 +1000

    init

--- SMSBOOOMMM/app/src/main/java/svenboom/smsbooommm/MainActivity.java
+++ SMSBOOOMMM/app/src/main/java/svenboom/smsbooommm/MainActivity.java
@@ -1,6 +1,7 @@
 package svenboom.smsbooommm;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
 import android.view.View;
@@ -12,12 +13,13 @@ public class MainActivity extends AppCompatActivity {
 
     int numberOfMessages = 5;
     int i = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btnSendSMS = (Button) findViewById(R.id.SendSMS);
-        TextView displayMessage = (TextView)findViewById(R.id.DisplayText);
+        TextView displayMessage = (TextView) findViewById(R.id.DisplayText);
 
 
         btnSendSMS.setOnClickListener(new View.OnClickListener() {
@@ -34,27 +36,28 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void sendSMS() throws InterruptedException {
-        TextView messageTime = (TextView)findViewById(R.id.MessageTime);
+        TextView messageTime = (TextView) findViewById(R.id.MessageTime);
         numberOfMessages = Integer.parseInt(messageTime.getText().toString());
-        for(i = 0; i<numberOfMessages;i++){
-                    messageSending();
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        for (i = 0; i < numberOfMessages; i++) {
+            messageSending();
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                public void run() {
+
+                }
+            }, 1000);
         }
     }
 
-    private void messageSending(){
+    private void messageSending() {
         SmsManager sms = SmsManager.getDefault();
-        EditText phoneNumberEdit = (EditText)findViewById(R.id.PhoneNumber);
-        TextView displayMessage = (TextView)findViewById(R.id.DisplayText);
+        EditText phoneNumberEdit = (EditText) findViewById(R.id.PhoneNumber);
+        TextView displayMessage = (TextView) findViewById(R.id.DisplayText);
         String phoneNumberStr = phoneNumberEdit.getText().toString();
 
 
         String message = "Test message" + i;
-        displayMessage.setText("Message had sent: " + i );
+        displayMessage.setText("Message had sent: " + i);
         sms.sendTextMessage(phoneNumberStr, null, message, null, null);
     }
 }
